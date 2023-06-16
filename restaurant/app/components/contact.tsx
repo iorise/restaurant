3350ce65da5ab9c7d0fb331f53225413e7db25da
NeroClaudiusuuu <tuajoz123@gmail.com>
2023-06-16 13:16:07 +0700

    change about to contact

---
+++ restaurant/app/components/contact.tsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <div>
+      <div className="relative">
+      <div className="">
+        <img src="/images/bg-restaurant.png" className="absolute" alt="" />
+      </div>
+      <div className="content-menu text-main-fourth">
+        <div className="bg-menu bg-main-bgMain bg-opacity-20 pl-5 pr-5 rounded-lg ">
+          <div className="card">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
